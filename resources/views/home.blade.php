66237a6fdaa2734b1517f0f7413d275ca2d92579
unknown <muhammadelanghardifal>
2021-08-20 09:46:07 +0700

    membuat aplikasi e-learnig untuk sekolah

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main')
+
+@section('container')
+<section class="head text-center my-5">
+    <h1 class="fw-bold">Welcome to SMP-SMK  Ikhwani</h1>
+    <h5 class="bg-light d-inline rounded shadow-sm px-3">Pembelajaran online dengan system yang terstruktur, untuk menambah pengetahuan.</h5>
+    <br>
+    <button class="btn login-now text-center px-5 py-3 mt-5 shadow">Login Now!</button>
+</section>  
+
+<section class="program row p-5">
+
+    <div class="col-md-3">
+        <h4 class="text-light">Programs</h4>
+        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, eius?</p>
+    </div>
+    <div class="col-md">
+    <h4 class="text-light">Setting</h4>
+    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, eius?</p>
+</div>
+     <div class="col-md">
+         <h4 class="text-light">Setting</h4>
+         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, eius?</p>
+  </div>
+  <div class="col-md">
+      <h4 class="text-light">Setting</h4>
+    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, eius?</p>
+</div>
+  
+</section>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light fw-bold px-5 position-fixed w-100" style="top: 0;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+        <ul class="ms-auto navbar-nav">
+                <li class="nav-item">
+            <a class="nav-link btn d-inline login-now px-3" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
