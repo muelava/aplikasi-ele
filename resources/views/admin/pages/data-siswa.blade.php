94defe8d768fb2a9a6a44c9eb46ee82dea7baec0
muelava <muelava@gmail.com>
2022-12-25 12:09:59 +0700

    fix bug

--- resources/views/admin/pages/data-siswa.blade.php
+++ resources/views/admin/pages/data-siswa.blade.php
@@ -63,7 +63,7 @@
                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye mr-50"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
                                             <span>Lihat</span>
                                           </a>
-                                          <a class="dropdown-item" href="/administrator/data-siswa/hapus/{{ $siswa->id }}" onclick="return confirm('Apakah anda yakin ingin manghapus data guru {{ $siswa->nama }}?')">
+                                          <a class="dropdown-item" href="/administrator/data-siswa/hapus/{{ $siswa->id }}" onclick="return confirm('Apakah anda yakin ingin manghapus data siswa {{ $siswa->nama }}?')">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash mr-50"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
                                             <span>Hapus</span>
                                           </a>
