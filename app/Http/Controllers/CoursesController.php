3d0f59fbbf0dbcf05c9facb5951caf399cc9b087
muelava <muelava@gmail.com>
2023-01-15 10:09:11 +0700

    materi siswa

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guru;
+use App\Models\SiswaModel;
+use App\Models\Kelas;
+use App\Models\Materi;
+use App\Models\Mapel;
+use App\Models\Tugas;
+use App\Models\Jadwal;
+use App\Models\Diskusi;
+use App\Models\SubDiskusi;
+
 use Illuminate\Http\Request;
 
 class CoursesController extends Controller
 {
     public function index()
     {
+        $siswa = SiswaModel::where('id', auth()->id())->first();
+        $materis = Materi::where('kelas_id', $siswa->kelas_id)->get();
+        
         return view('courses.index', [
-            'active' => 'course'
+            'active' => 'course',
+            'materis' => $materis,
         ]);
     }
 }
--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -16,28 +16,43 @@
   <div class="content-overlay"></div>
   <div class="header-navbar-shadow"></div>
   <div class="content-wrapper">
+
     <div class="content-header row">
+      <div class="content-header-left col-md-9 col-12 mb-2">
+        <div class="row breadcrumbs-top">
+          <div class="col-12">
+            <h2 class="content-header-title float-left mb-0">Courses</h2>
+          </div>
+        </div>
+      </div>
     </div>
+
     <div class="content-body">
       <!-- Dashboard Ecommerce Starts -->
       <section id="dashboard-ecommerce">
         <div class="card">
           <div class="col-12 row mx-0 p-2">
-            <a href="javascript:" class="col-xl-2 col-md-4 col-sm-6 shadow" title="Klik info untuk membuka materi">
-              <div class="card text-left">
-                <small class="d-block text-secondary mt-1 text-right" style="font-size:0.8rem">waktu</small>
-                <div class="card-body p-1">
-                  <div class="avatar bg-light-primary p-50 mb-1">
-                    <div class="avatar-content">
-                      <i data-feather="file-text" class="font-medium-5"></i>
+            @if ($materis->count() > 0)
+              @foreach ($materis as $materi)
+              <a href="javascript:" class="col-xl-2 col-md-4 col-sm-6 shadow" title="Klik info untuk membuka materi">
+                <div class="card text-left">
+                  <small class="d-block text-secondary mt-1 text-right" style="font-size:0.8rem">{{ $materi->created_at->diffForHumans() }}</small>
+                  <div class="card-body p-1">
+                    <div class="avatar bg-light-primary p-50 mb-1">
+                      <div class="avatar-content">
+                        <i data-feather="file-text" class="font-medium-5"></i>
+                      </div>
                     </div>
+                    <h5 class="font-weight-bolder text-uppercase">{{ $materi->mata_pelajaran->mapel }}</h5>
+                    <p class="mb-0">{{ $materi->materi }}</p>
+                    <small class="card-text text-dark mb-0">{{ $materi->deskripsi }}</small>
                   </div>
-                  <h5 class="font-weight-bolder text-uppercase">SMK</h5>
-                  <p class="mb-0">mata pelajaran</p>
-                  <small class="card-text text-dark mb-0">nama materi</small>
                 </div>
-              </div>
-            </a>
+              </a>
+              @endforeach
+            @else
+              <p class="h5 mx-auto">Tidak Ada Materi <b>{{ request('materi') }}</b></p>
+            @endif
           </div>
         </div>
       </section>
